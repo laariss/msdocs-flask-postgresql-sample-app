d59092ec52fa11798daffe8070bfde13c3cdce91
Larisa Sas <74658540+laariss@users.noreply.github.com>
2025-06-19 21:04:14 +0000

    mini-cambio

--- azureproject/production.py
+++ azureproject/production.py
@@ -6,6 +6,3 @@ DATABASE_URI = 'postgresql+psycopg2://{dbuser}:{dbpass}@{dbhost}/{dbname}'.forma
      dbhost=os.getenv('AZURE_POSTGRESQL_HOST'),
      dbname=os.getenv('AZURE_POSTGRESQL_NAME')
 )
-
-from flask_sqlalchemy import SQLAlchemy
-db = SQLAlchemy()
