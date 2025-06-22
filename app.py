0f66fa4455e9ab5761a8918a433bf62d31fe8c92
Larisa Sas <74658540+laariss@users.noreply.github.com>
2025-06-22 15:29:32 +0000

    importación

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import Flask, request, render_template, redirect, url_for, send_from_
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_wtf.csrf import CSRFProtect
+from flask import Flask, request, jsonify
 
 app = Flask(__name__, static_folder='static')
 csrf = CSRFProtect(app)
@@ -32,7 +33,18 @@ def index():
     pixel_counts = PixelCount.query.order_by(PixelCount.timestamp.desc()).all()
     return render_template("index.html", pixel_counts=pixel_counts)
 
-@app.route("/add_pixel", methods=["POST"])
+@app.route('/add_pixel', methods=['POST'])
+def add_pixel():
+    data = request.json
+    print(data)
+    return jsonify({"status": "ok"}), 200
+
+if __name__ == '__main__':
+    import os
+    port = int(os.environ.get('PORT', 5000))  # Usa el puerto que Azure asigna
+    app.run(host='0.0.0.0', port=port)
+
+"""@app.route("/add_pixel", methods=["POST"])
 @csrf.exempt
 def add_pixel():
     if not request.is_json:
@@ -56,7 +68,7 @@ def add_pixel():
     db.session.add(nuevo)
     db.session.commit()
     return {"id": nuevo.id}, 201
-
+"""
 @app.route("/pixelcounts", methods=["GET"])
 def get_pixelcounts():
     registros = PixelCount.query.order_by(PixelCount.timestamp.desc()).all()
